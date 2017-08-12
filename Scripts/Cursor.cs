fa14202ea3d6d9aab7995ace9e35d6890766e9ff
Jake Landry <jake@jakelandry.com>
2017-08-11 17:56:56 -0700

    commit 8/11

---
+++ Scripts/Cursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+
+	CameraRaycaster cameraRaycaster;
+
+	void Start () {
+		cameraRaycaster = GetComponent<CameraRaycaster> ();
+	}
+
+	void Update() {
+		if (Input.GetMouseButton (0)) {
+			//Debug.Log (cameraRaycaster.layerHit);
+		}
+	}
+
+
+}
