f7214a333bc990277cc57499b9c100c234f14e51
Jake Landry <jake@jakelandry.com>
2017-08-14 23:36:23 -0700

    commit 8/14

--- Player/PlayerMovement.cs
+++ Player/PlayerMovement.cs
@@ -5,11 +5,14 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+
 	[SerializeField] float walkMoveStopRadius = 0.2f;
 
 	ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
+
+	private bool isInDirectMode = false; //TODO: Consider making static.
 
 
     private void Start()
@@ -22,9 +25,39 @@ public class PlayerMovement : MonoBehaviour
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
-        {
-            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+		if (Input.GetKeyDown (KeyCode.G)) { // TODO: Allow player to remap later.
+			isInDirectMode = !isInDirectMode;
+		}
+
+		if (isInDirectMode) {
+			ProcessDirectMovement ();
+
+		} else {
+			ProcessMouseMovement ();
+
+		}
+			
+    }
+
+	private void ProcessDirectMovement(){
+		print ("Direct movement.");
+
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+
+		Vector3 m_CamForward = Vector3.Scale (Camera.main.transform.forward, new Vector3 (1, 0, 1)).normalized;
+		Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+		m_Character.Move (m_Move, false, false);
+	}
+
+	private void ProcessMouseMovement(){
+
+		print ("Mouse movement.");
+		if (Input.GetMouseButton(0))
+		{
+			print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
 
 			switch (cameraRaycaster.layerHit) {
 			case Layer.Walkable:
@@ -37,7 +70,7 @@ public class PlayerMovement : MonoBehaviour
 				print ("Shouldn't be here!");
 				return;
 			}
-        }
+		}
 		var playerToClickPoint = currentClickTarget - transform.position;
 		if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
 			m_Character.Move (playerToClickPoint, false, false);
@@ -45,7 +78,6 @@ public class PlayerMovement : MonoBehaviour
 			m_Character.Move (Vector3.zero, false, false);
 
 		}
-			
-    }
+	}
 }
 
